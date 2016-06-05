2a075863bac09c931497a37ec101c3cf3a9b8b31
perlancar (@pc-home) <perlancar@gmail.com>
2016-06-05 13:18:15 +0700

    Initial commit

---
+++ lib/Acme/MetaSyntactic/dangdut.pm
@@ -0,0 +1,34 @@
+package Acme::MetaSyntactic::dangdut;
+
+# DATE
+# VERSION
+
+use parent qw(Acme::MetaSyntactic::MultiList);
+__PACKAGE__->init;
+
+1;
+# ABSTRACT: A selection of popular Indonesian dangdut singers
+
+=head1 SYNOPSIS
+
+ % perl -MAcme::MetaSyntactic=dangdut -le 'print metaname'
+ rhoma
+
+ % meta dangdut 2
+ gotik
+ cita
+
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>
+
+=cut
+
+__DATA__
+# default
+:all
+# names first
+rhoma elvy rita cita mansyur hamdan meggy muchsin itje jaja camelia iis evie mega vety cucu cici lilis nini fitri annisa juwita inul alam ria sazkia ayu jenita alam
+# names last
+irama sukaesih sugiarto citata alatas trisnawati miharja malik dahlia tamala mustika vera cahyati paramida karlina carlina bahar daratista amelia gotik tingting janet
